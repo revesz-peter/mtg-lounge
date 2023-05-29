2649fa8ef0280f6542b5af4711eb2c05391e803f
Péter Révész <reveszpeter21@gmail.com>
2023-05-29 19:52:57 +0200

    fix issue with multiple card addition not rendering in deck

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,6 @@ import { useDeckManager } from "./hooks/useDeckManager";
 import DeckCard from "./components/DeckCard";
 import ColorFilter from "./components/ColorFilter";
 
-
 export interface CardType {
     id: string;
     imageUris: string;
@@ -66,30 +65,17 @@ function App() {
     };
 
     const addToDeck = (card: CardType) => {
-        const existingCardIndex = deck.findIndex(
-            (deckCard) => deckCard.id === card.id
-        );
-        if (existingCardIndex > -1) {
-            setDeckCounts((prevCounts) => {
-                const updatedCounts = {
-                    ...prevCounts,
-                    [card.id]: (prevCounts[card.id] || 0) + 1,
-                };
-                return updatedCounts;
-            });
-        } else {
-            setDeck((prevDeck) => {
-                const updatedDeck = [...prevDeck, card];
-                return updatedDeck;
-            });
-            setDeckCounts((prevCounts) => {
-                const updatedCounts = {
-                    ...prevCounts,
-                    [card.id]: 1,
-                };
-                return updatedCounts;
-            });
-        }
+        setDeck((prevDeck) => {
+            const updatedDeck = [...prevDeck, card];
+            return updatedDeck;
+        });
+        setDeckCounts((prevCounts) => {
+            const updatedCounts = {
+                ...prevCounts,
+                [card.id]: (prevCounts[card.id] || 0) + 1,
+            };
+            return updatedCounts;
+        });
     };
 
     const renderCards = (cards: CardType[]) => {
@@ -116,7 +102,7 @@ function App() {
             <section className="max-w-7xl mx-auto flex">
                 <div className="w-4/5">
                     <div className="mt-5">
-                      <ColorFilter/>
+                        <ColorFilter />
                         <Searchfield
                             labelName="Search cards"
                             type="text"
@@ -144,18 +130,22 @@ function App() {
                     style={{ backgroundColor: isOver ? "lightblue" : "white" }}
                     className="w-1/3 border-2 border-gray-300 h-screen ml-4 m-7"
                 >
-                    {Object.entries(deckCounts).map(([id, count]) => {
-                        const card = allCards.find((card) => card.id === id);
-                        return card ? (
-                            <DeckCard
-                                key={card.id}
-                                imageUris={card.imageUris}
-                                id={card.id}
-                                name={card.name}
-                                count={count}
-                            />
-                        ) : null;
-                    })}
+                    {Array.from(new Set(deck.map((card) => card.id))).map(
+                        (id) => {
+                            const card = allCards.find(
+                                (card) => card.id === id
+                            );
+                            return card ? (
+                                <DeckCard
+                                    key={card.id}
+                                    imageUris={card.imageUris}
+                                    id={card.id}
+                                    name={card.name}
+                                    count={deckCounts[id]}
+                                />
+                            ) : null;
+                        }
+                    )}
                 </div>
             </section>
         </div>
