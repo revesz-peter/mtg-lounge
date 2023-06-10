bc27c5eb8ea72ee8b24fd5303594dc0edc93b807
Péter Révész <reveszpeter21@gmail.com>
2023-06-10 10:56:44 +0200

    improve card inspection from the deck

--- client/src/components/DeckCard.tsx
+++ client/src/components/DeckCard.tsx
@@ -18,8 +18,11 @@ const DeckCard: React.FC<DeckCardProps> = ({ id, imageUris, name, count }) => {
     }));
 
     const [showCard, setShowCard] = useState(false);
+    const [cardPosition, setCardPosition] = useState({ top: 0, left: 0 });
 
-    const handleMouseEnter = () => {
+    const handleMouseEnter = (e: React.MouseEvent) => {
+        const rect = (e.currentTarget as HTMLElement).getBoundingClientRect();
+        setCardPosition({ top: rect.top, left: rect.left - 350 });
         setShowCard(true);
     };
 
@@ -27,13 +30,18 @@ const DeckCard: React.FC<DeckCardProps> = ({ id, imageUris, name, count }) => {
         setShowCard(false);
     };
 
+    const handleMouseDown = () => {
+        setShowCard(false);
+    };
+
     return (
         <div
             ref={drag}
             style={{ opacity: isDragging ? 0.5 : 1 }}
-            className="flex flex-col items-center p-0.5"
+            className="relative flex flex-col items-center p-0.5"
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
+            onMouseDown={handleMouseDown}
         >
             <div className="bg-gray-200 rounded flex w-full">
                 <div className="flex justify-between items-center w-full">
@@ -45,7 +53,20 @@ const DeckCard: React.FC<DeckCardProps> = ({ id, imageUris, name, count }) => {
                     </span>
                 </div>
             </div>
-            {showCard && <img src={imageUris} alt={name} />}
+            {showCard && (
+                <div 
+                style={{
+                    position: "fixed", 
+                    top: cardPosition.top, 
+                    left: cardPosition.left, 
+                    zIndex: 1,
+                    width: '350px',
+                    height: '350px'
+                }}
+                >
+                    <img src={imageUris} alt={name} className="rounded-lg"/>
+                </div>
+            )}
         </div>
     );
 };
